75263029ca69f09839a3920761a200f99796dd98
cremaluca <acerkacke@gmail.com>
2016-01-21 11:38:04 +0100

    Sistemati i problemi di path ma non tutti

--- SupermercatoProdotti/src/Cramest/utils/SalvaFile.java
+++ SupermercatoProdotti/src/Cramest/utils/SalvaFile.java
@@ -1,8 +1,18 @@
 
 package Cramest.utils;
 
+import java.util.Arrays;
+
 public class SalvaFile {
-	public static void salva(int numero,String nomeFile){
+	String nomeFile;
+	
+	public void salva(int numero){
 		
+	}
+	
+	private void scriviSuFile(){
+		List<String> lines = Arrays.asList("The first line", "The second line");
+		Path file = Paths.get("the-file-name.txt");
+		Files.write(file, lines, Charset.forName("UTF-8"));
 	}
 }
