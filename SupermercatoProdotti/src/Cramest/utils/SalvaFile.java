825eaf1668ba5788c73ec3b3f340a99c9244c01b
Acerkacke <acerkacke@gmail.com>
2016-01-22 12:03:40 +0100

    Sistemato completamente il salvafile

--- SupermercatoProdotti/src/Cramest/utils/SalvaFile.java
+++ SupermercatoProdotti/src/Cramest/utils/SalvaFile.java
@@ -1,7 +1,6 @@
 
 package Cramest.utils;
 
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -9,52 +8,57 @@ import java.io.IOException;
 
 public class SalvaFile {
 	private String pathFile;
-	
-	public SalvaFile(String path){
+
+	public SalvaFile(String path) {
 		pathFile = path;
 	}
-	public String getPath(){
+
+	public String getPath() {
 		return pathFile;
 	}
-	public boolean salva(int numero){
+
+	public boolean salva(int numero) {
 		return scriviSuFile(numero);
 	}
-	public boolean salva(double numero){
+
+	public boolean salva(double numero) {
 		return scriviSuFile(numero);
 	}
-	public boolean salva(String valore){
+
+	public boolean salva(float numero) {
+		return scriviSuFile(numero);
+	}
+
+	public boolean salva(String valore) {
 		return scriviSuFile(valore);
 	}
-	private boolean scriviSuFile(int n){
+
+	private boolean scriviSuFile(int n) {
 		return scriviSuFile(String.valueOf(n));
 	}
-	
-	private boolean scriviSuFile(double d){
+
+	private boolean scriviSuFile(double d) {
 		return scriviSuFile(String.valueOf(d));
 	}
-	
-	private boolean scriviSuFile(float f){
+
+	private boolean scriviSuFile(float f) {
 		return scriviSuFile(String.valueOf(f));
 	}
-	
-	private boolean scriviSuFile(String content){
+
+	private boolean scriviSuFile(String content) {
 		try {
 			File file = new File(pathFile);
-
 			if (!file.exists()) {
 				file.createNewFile();
 			}
-
 			FileWriter fw = new FileWriter(file.getAbsoluteFile());
 			BufferedWriter bw = new BufferedWriter(fw);
 			bw.write(content);
 			bw.close();
-
 			return true;
-
 		} catch (IOException e) {
-			e.printStackTrace();
 			return false;
 		}
+
 	}
 }
