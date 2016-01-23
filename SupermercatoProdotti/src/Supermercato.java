050f4d7573ad4e3306a24eb5b2263aa99e885d02
davidenardi <sampras98@gmail.com>
2016-01-23 09:22:43 +0100

    Creato AggiungiProdotto

--- SupermercatoProdotti/src/Supermercato.java
+++ SupermercatoProdotti/src/Supermercato.java
@@ -1,6 +1,8 @@
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Label;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.wb.swt.SWTResourceManager;
 
@@ -9,28 +11,20 @@ import Cramest.Prodotti.*;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.MouseTrackAdapter;
-import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.events.ShellAdapter;
-import org.eclipse.swt.events.ShellEvent;
-import org.eclipse.swt.events.DisposeListener;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.PaintListener;
-import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.events.MouseMoveListener;
 import org.eclipse.swt.events.FocusAdapter;
 import org.eclipse.swt.events.FocusEvent;
 
 
 public class Supermercato {
 
-	protected Shell shell;
+	protected Shell shlSupermercato;
 	private static Text text_prezzo;
 	private static Text text_codice;
 	private static ListaSpesa carrello;
@@ -43,6 +37,8 @@ public class Supermercato {
 			new Alimentare("4164", "Una Banana", 0.20, new Data(20, 1, 2016)),
 			new NonAlimentare("4324", "Carta igenica", 1.00, "carta"),
 			new NonAlimentare("6427", "Bottiglia d'acqua", 1.00, "plastica"),
+			new NonAlimentare("9547", "Batteria", 1.00, "metallo pesante"),
+			new NonAlimentare("9547", "Batteria", 1.00, "metallo pesante"),
 			new NonAlimentare("9547", "Batteria", 1.00, "metallo pesante"),
 			};
 	private Text text_totale;
@@ -63,9 +59,9 @@ public class Supermercato {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlSupermercato.open();
+		shlSupermercato.layout();
+		while (!shlSupermercato.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -78,15 +74,17 @@ public class Supermercato {
 			listaAggiornata[i] = carrello.getProdotto(i).toString();
 		}
 		list.setItems(listaAggiornata);
+		text_totale.setText(String.valueOf(Math.floor(carrello.calcolaTOT()*1000)/1000));
+		
 	}
 	
 	private void vaiAllaCassa(){
-		shell.setSize(shell.getSize().x,400);
+		shlSupermercato.setSize(shlSupermercato.getSize().x,400);
 	}
 	
 	private void paga(boolean haTessera){
 		if(!isUscito){
-			shell.setSize(shell.getSize().x,450);
+			shlSupermercato.setSize(shlSupermercato.getSize().x,450);
 			if(haTessera)
 				carrello.applicaSconti();
 			text_scontrino.setText(String.valueOf(carrello.calcolaTOT()));
@@ -95,18 +93,18 @@ public class Supermercato {
 	}
 
 	protected void createContents() {
-		shell = new Shell();
-		shell.setMinimumSize(new Point(460, 305));
-		shell.setSize(460, 305);
-		shell.setText("SWT Application");
+		shlSupermercato = new Shell();
+		shlSupermercato.setMinimumSize(new Point(460, 305));
+		shlSupermercato.setSize(460, 305);
+		shlSupermercato.setText("Supermercato");
 		
-		Label lblSupermercato = new Label(shell, SWT.CENTER);
+		Label lblSupermercato = new Label(shlSupermercato, SWT.CENTER);
 		lblSupermercato.setFont(SWTResourceManager.getFont("Segoe UI Semibold", 13, SWT.BOLD));
 		lblSupermercato.setAlignment(SWT.CENTER);
 		lblSupermercato.setBounds(0, 0, 434, 23);
 		lblSupermercato.setText("SUPERMERCATO");
 		
-		ScrolledComposite scrolledComposite = new ScrolledComposite(shell, SWT.BORDER | SWT.V_SCROLL);
+		ScrolledComposite scrolledComposite = new ScrolledComposite(shlSupermercato, SWT.BORDER | SWT.V_SCROLL);
 		scrolledComposite.setAlwaysShowScrollBars(true);
 		scrolledComposite.setExpandVertical(true);
 		scrolledComposite.setExpandHorizontal(true);
@@ -117,23 +115,23 @@ public class Supermercato {
 		scrolledComposite.setContent(list);
 		scrolledComposite.setMinSize(list.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 		
-		Label lblNewLabel = new Label(shell, SWT.NONE);
+		Label lblNewLabel = new Label(shlSupermercato, SWT.NONE);
 		lblNewLabel.setBounds(221, 26, 55, 21);
 		lblNewLabel.setText("Prezzo");
 		
-		text_prezzo = new Text(shell, SWT.BORDER);
+		text_prezzo = new Text(shlSupermercato, SWT.BORDER);
 		text_prezzo.setEditable(false);
 		text_prezzo.setBounds(348, 23, 76, 21);
 		
-		Label lblCodice = new Label(shell, SWT.NONE);
+		Label lblCodice = new Label(shlSupermercato, SWT.NONE);
 		lblCodice.setBounds(221, 53, 55, 15);
 		lblCodice.setText("Codice");
 		
-		text_codice = new Text(shell, SWT.BORDER);
+		text_codice = new Text(shlSupermercato, SWT.BORDER);
 		text_codice.setEditable(false);
 		text_codice.setBounds(348, 50, 76, 21);
 		
-		Button btnTotale = new Button(shell, SWT.NONE);
+		Button btnTotale = new Button(shlSupermercato, SWT.NONE);
 		btnTotale.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -143,15 +141,15 @@ public class Supermercato {
 		btnTotale.setBounds(221, 236, 203, 25);
 		btnTotale.setText("Vai alla cassa");
 		
-		Label lblTotale = new Label(shell, SWT.NONE);
+		Label lblTotale = new Label(shlSupermercato, SWT.NONE);
 		lblTotale.setBounds(10, 241, 55, 13);
 		lblTotale.setText("Totale : ");
 		
-		text_totale = new Text(shell, SWT.BORDER);
+		text_totale = new Text(shlSupermercato, SWT.BORDER);
 		text_totale.setEditable(false);
 		text_totale.setBounds(71, 238, 124, 21);
 		
-		btnNo = new Button(shell, SWT.NONE);
+		btnNo = new Button(shlSupermercato, SWT.NONE);
 		btnNo.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -162,7 +160,7 @@ public class Supermercato {
 		formToolkit.adapt(btnNo, true, true);
 		btnNo.setText("No");
 		
-		btnSi = new Button(shell, SWT.NONE);
+		btnSi = new Button(shlSupermercato, SWT.NONE);
 		btnSi.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -173,22 +171,22 @@ public class Supermercato {
 		formToolkit.adapt(btnSi, true, true);
 		btnSi.setText("Si");
 		
-		Label lblHaiLaCarta = new Label(shell, SWT.NONE);
+		Label lblHaiLaCarta = new Label(shlSupermercato, SWT.NONE);
 		lblHaiLaCarta.setBounds(179, 300, 110, 15);
 		formToolkit.adapt(lblHaiLaCarta, true, true);
 		lblHaiLaCarta.setText("Hai la carta fedelt\u00E0?");
 		
-		Label lblAlloraFanno = new Label(shell, SWT.NONE);
+		Label lblAlloraFanno = new Label(shlSupermercato, SWT.NONE);
 		lblAlloraFanno.setBounds(120, 383, 75, 15);
 		formToolkit.adapt(lblAlloraFanno, true, true);
 		lblAlloraFanno.setText("Allora fanno :");
 		
-		text_scontrino = new Text(shell, SWT.BORDER);
+		text_scontrino = new Text(shlSupermercato, SWT.BORDER);
 		text_scontrino.setEditable(false);
 		text_scontrino.setBounds(248, 380, 76, 21);
 		formToolkit.adapt(text_scontrino, true, true);
 		
-		ScrolledComposite scrolledComposite_1 = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		ScrolledComposite scrolledComposite_1 = new ScrolledComposite(shlSupermercato, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		scrolledComposite_1.setBounds(221, 72, 213, 158);
 		formToolkit.adapt(scrolledComposite_1);
 		formToolkit.paintBordersFor(scrolledComposite_1);
